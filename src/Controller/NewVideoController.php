dd594d3af9240adf00d2a437e88b6ab34dd8dfd7
Aldair Meneses <aldair.meneses@protonmail.com>
2023-04-18 13:18:33 -0300

    Adiciona upload de imagens + tratamento

--- src/Controller/NewVideoController.php
+++ src/Controller/NewVideoController.php
@@ -26,7 +26,22 @@ class NewVideoController implements Controller
             return;
         }
 
-        $success = $this->videoRepository->add(new Video($url, $titulo));
+        $video = new Video($url, $titulo);
+        if ($_FILES['image']['error'] === UPLOAD_ERR_OK) {
+            $safeFileName = uniqid('upload_') . '_' . pathinfo($_FILES['image']['name'], PATHINFO_BASENAME);
+            $finfo = new \finfo(FILEINFO_MIME_TYPE);
+            $mimeType = $finfo->file($_FILES['image']['tmp_name']);
+
+            if(str_starts_with($mimeType, 'image/')) {
+                move_uploaded_file(
+                    $_FILES['image']['tmp_name'],
+                    __DIR__ . '/../../public/img/uploads/' . $safeFileName
+            );
+            $video->setFilePath($_FILES['image']['name']);
+            } 
+        }
+
+        $success = $this->videoRepository->add($video);
         if ($success === false) {
             header('Location: /?sucesso=0');
         } else {
